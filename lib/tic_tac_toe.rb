d5bb7e19dec1b696a7099da34127951c308f0ef8
Becky Zorn <bczorn@gmail.com>
2019-10-25 14:27:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,18 +44,7 @@ class TicTacToe
     index.between?(0, 8) && !position_taken?(index)
   end
   
-  def turn
-    puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
-    if valid_move?(index) == true
-      move(index, current_player)
-      @board
-    else
-      puts "Invalid"
-      turn
-    end
-  end
+ 
   
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
